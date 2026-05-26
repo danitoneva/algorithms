9744d443cf4c7d97c9881aaee8a2f6661328aec0
danitoneva <d.toneva2003@gmail.com>
2026-05-26 13:48:04 +0300

    updating the function are_anagrams

--- src/are_anagrams.py
+++ src/are_anagrams.py
@@ -2,21 +2,23 @@
 This module checks whether two strings are anagrams.
 """
 
+
 class AnagramModule:
     """
     This class checks whether two strings are anagrams.
     """
 
     @staticmethod
-    def are_anagrams(string1: str, string2: str) -> str:
+    def are_anagrams(string1: str, string2: str) -> bool:
         """
         Checks for anagrams.
 
         :parameter string1: first string
         :parameter string2: second string
         """
-        reversed_string2 = string2[::-1]
-        if string1 == reversed_string2:
-            print("The two strings are anagrams.")
+        sorted_string1 = sorted(string1)
+        sorted_string2 = sorted(string2)
+        if sorted_string1 == sorted_string2:
+            return True
         else:
-            print("The two strings aren't anagrams.")
+            return False
