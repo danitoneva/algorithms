870fe2ebd06e99195e6b8b61c72ceec650604bb8
danitoneva <d.toneva2003@gmail.com>
2026-05-26 16:39:42 +0300

    updating the file names

--- src/is_anagram.py
+++ src/is_anagram.py
@@ -9,23 +9,23 @@ class AnagramModule:
     """
 
     @staticmethod
-    def are_anagrams(string1: str, string2: str) -> bool:
+    def is_anagram(string1: str, string2: str) -> bool:
         """
         Checks for anagrams.
 
         :parameter string1: first string
         :parameter string2: second string
         """
-        string1 = string1.lower()
-        string2 = string2.lower()
-        string1 = "".join(string1.split())
-        string2 = "".join(string2.split())
-        sorted_string1 = sorted(string1)
-        sorted_string2 = sorted(string2)
         if string1.isalpha() and string2.isalpha():
+            string1 = string1.lower()
+            string2 = string2.lower()
+            string1 = "".join(string1.split())
+            string2 = "".join(string2.split())
+            sorted_string1 = sorted(string1)
+            sorted_string2 = sorted(string2)
             if sorted_string1 == sorted_string2:
                 return True
             else:
                 return False
-        else:
+        else: 
             return False
--- tests/test_is_anagram.py
+++ tests/test_is_anagram.py
@@ -2,7 +2,7 @@
 Test for my anagram module.
 """
 import unittest
-from src.are_anagrams import AnagramModule
+from src.is_anagram import AnagramModule
 
 
 class TestAnagramModule(unittest.TestCase):
@@ -12,25 +12,30 @@ class TestAnagramModule(unittest.TestCase):
 
     def test_two_strings_anagrams(self):
         expected_result = True
-        actual_result = AnagramModule.are_anagrams(string1= "string", string2= "irgnts")
+        actual_result = AnagramModule.is_anagram(string1= "string", string2= "irgnts")
         self.assertEqual(expected_result, actual_result)
 
     def test_two_strings_anagrams_capital_letter(self):
         expected_result = True
-        actual_result = AnagramModule.are_anagrams(string1= "String", string2= "irgnTs")
+        actual_result = AnagramModule.is_anagram(string1= "String", string2= "irgnTs")
         self.assertEqual(expected_result, actual_result)
 
     def test_two_strings_anagrams_with_space(self):
-        expected_result = True
-        actual_result = AnagramModule.are_anagrams(string1= "st ring", string2= "string")
+        expected_result = False
+        actual_result = AnagramModule.is_anagram(string1= "st ring", string2= "string")
         self.assertEqual(expected_result, actual_result)
 
     def test_two_strings_anagrams_with_space_and_capital(self):
-        expected_result = True
-        actual_result = AnagramModule.are_anagrams(string1= "sT ring", string2= "stRi ng")
+        expected_result = False
+        actual_result = AnagramModule.is_anagram(string1= "sT ring", string2= "stRi ng")
         self.assertEqual(expected_result, actual_result)
 
     def test_two_strings_anagrams_with_symbol(self):
         expected_result = False
-        actual_result = AnagramModule.are_anagrams(string1= "string)", string2= "str*ing")
+        actual_result = AnagramModule.is_anagram(string1= "string)", string2= "str*ing")
+        self.assertEqual(expected_result, actual_result)
+
+    def test_two_strings_anagrams_with_digits(self):
+        expected_result = False
+        actual_result = AnagramModule.is_anagram(string1= "string9", string2= "str5ing")
         self.assertEqual(expected_result, actual_result)
