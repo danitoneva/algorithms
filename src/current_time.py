1c8e6fdbcfa7927b1965a977e3c60875df3f32f1
danitoneva <d.toneva2003@gmail.com>
2026-05-27 13:27:17 +0300

    creating algorithms for subtracting dates

--- src/current_time.py
+++ src/current_time.py
@@ -8,9 +8,11 @@ class CurrentTime:
     "This class gets the current time in python."
 
     @staticmethod 
-    def get_current_time(time: int) -> str:
+    def get_current_time(current_time: int) -> str:
         """
         Get current time.
+
+        :parameter:time
         """
         current_time = datetime.now()
         return current_time.strftime("%H:%M:%S.%f")[:-6]
