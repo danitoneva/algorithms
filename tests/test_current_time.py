4210b4b4b247928620d45dc1ab0a2dfe0d1ca26d
danitoneva <d.toneva2003@gmail.com>
2026-05-28 10:58:21 +0300

    updating unittest - current_time

--- tests/test_current_time.py
+++ tests/test_current_time.py
@@ -2,11 +2,19 @@
 Test for my current time module.
 """
 import unittest
+from datetime import datetime
 from src.current_time import CurrentTime
 
 
 class TestCurrentTime(unittest.TestCase):
     """This class test CurrentTime."""
 
-    def test_current_time_format(self):
-        raise NotImplementedError("Temporary fail.")
+    def test_hour_range(self):
+        current_time = datetime.now()
+        self.assertGreaterEqual(current_time.hour, 0, "Hour should be greater than 0.")
+        self.assertLessEqual(current_time.hour, 23, "Hour should be less than 24.")
+
+    def test_second_range(self):
+        current_time = datetime.now()
+        self.assertGreaterEqual(current_time.hour, 0, "Seconds should be greater than 0.")
+        self.assertLessEqual(current_time.hour, 23, "Seconds should be less than 59.")
