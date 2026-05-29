84fee86f6e7386ad83180f394a5a6068906e3cc4
danitoneva <d.toneva2003@gmail.com>
2026-05-29 09:54:32 +0300

    updating the function of fibonacci

--- src/fibonacci_numbers.py
+++ src/fibonacci_numbers.py
@@ -17,9 +17,4 @@ class FibonacciModule:
             raise ValueError("It should print the first 10 numbers.")
         if number < 0:
             raise ValueError("This number should be non-negative.")
-        numbers = []
-        first_number, second_number = 0, 1
-        for num in range(10):
-            numbers.append(first_number)
-            first_number, second_number = second_number, first_number + second_number
-        return numbers
+        return FibonacciModule.fibonacci(number - 1) + FibonacciModule.fibonacci(number -2 )
