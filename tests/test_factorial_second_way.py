8ee5b23406f9f33d647e6ab8939bcf1947996ea8
danitoneva <d.toneva2003@gmail.com>
2026-05-29 09:59:04 +0300

    updating test_factorial

--- tests/test_factorial_second_way.py
+++ tests/test_factorial_second_way.py
@@ -1,14 +1,13 @@
 """
 Test for my factorial module.
 """
-import pytest
 from src.factorial_second_way import SecondFactorialModule
 
 class TestSecondFactorialModule():
     """Class test FactorialModule"""
 
     def test_factorial_negative_number(self):
-        assert SecondFactorialModule.factorial_second(-1) 
+        assert SecondFactorialModule.factorial_second(-1)
 
     def test_factorial_with_one(self):
         assert SecondFactorialModule.factorial_second(0) == 1
