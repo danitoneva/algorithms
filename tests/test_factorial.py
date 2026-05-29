b41eca934eee55a0cd659343ce526e4d80c43f92
danitoneva <d.toneva2003@gmail.com>
2026-05-29 09:57:01 +0300

    updating test_factorial

--- tests/test_factorial.py
+++ tests/test_factorial.py
@@ -1,14 +1,13 @@
 """
 Test for my factorial module.
 """
-import pytest
 from src.factorial import FactorialModule
 
 class TestFactorialModule():
     """Class test FactorialModule"""
 
     def test_factorial_negative_number(self):
-        assert FactorialModule.factorial(-1) 
+        assert FactorialModule.factorial(-1)
 
     def test_factorial_with_one(self):
         assert FactorialModule.factorial(0) == 1
